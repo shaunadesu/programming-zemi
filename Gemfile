e79ab1523ed7740538c427667f91e3e25cf58c7b
shaunadesu <shaunajdesu@gmail.com>
2022-04-23 18:24:14 +0000

    login and logout

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.5.1'
 gem 'rails', '~> 5.2.1'
 gem 'bcrypt', '3.1.12'
 gem 'bootstrap-sass', '3.3.7'
+gem 'jquery-rails'
 # Use mysql as the database for Active Record
 # gem 'sqlite3'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
-  get 'home' => "static_pages/home"
-  get 'help' => "static_pages/help"
+  get 'home' => "static_pages#home"
+  get 'help' => "static_pages#help"
   get 'about' => "static_pages#about"
   get 'contact' => "static_pages#contact"
-  get 'signup' => "users#new"
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup' => "users#new"
+  get 'login' => "sessions#new"
+  post 'login' => "sessions#create"
+  delete 'logout' => "sessions#destroy"
+  resources :users
 end
